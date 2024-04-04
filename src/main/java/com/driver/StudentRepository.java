e08f8e557838ac057771af670726a473ee33e2cd
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-04-04 12:43:25 +0530

    Assignment

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,53 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        studentMap.put(student.getName(), student);
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        teacherMap.put(teacher.getName(),teacher);
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
             // your code goes here
+            List<String>studentList = teacherStudentMapping.getOrDefault(teacher, new ArrayList<>());
+            studentList.add(student);
+            teacherStudentMapping.put(teacher,studentList);
         }
     }
 
     public Student findStudent(String student){
         // your code goes here
+        return studentMap.get(student);
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        return teacherMap.get(teacher);
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+        return teacherStudentMapping.get(teacher);
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        return new ArrayList<>(studentMap.keySet());
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        teacherMap.remove(teacher);
+        teacherStudentMapping.remove(teacher);
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+        teacherMap.clear();
+        teacherStudentMapping.clear();
     }
 }
--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -46,4 +46,6 @@ public class StudentService {
     public void deleteAllTeachers(){
         studentRepository.deleteAllTeachers();
     }
+
+
 }
